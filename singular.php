84de063d7027f55114234947b2aef16a93cd644f
Gaurav Pareek <me@grv.rocks>
2016-01-03 11:18:54 +0530

    testing singular.php

--- singular.php
+++ singular.php
@@ -1,16 +1,15 @@
 <?php
 //* Backstretch
-
+$postid = get_the_ID();
 //* Localize backstretch script
 add_action( 'genesis_after_entry', 'grvrocks_set_background_image' );
 function grvrocks_set_background_image() {
-	$image = array( 'src' => has_post_thumbnail() ? genesis_get_image( array( 'format' => 'url' ) ) : '' );
+	$image = array( 'src' => has_post_thumbnail($postid) ? genesis_get_image( array( 'format' => 'url' ) ) : '' );
 	wp_localize_script( 'grvrocks-backstretch-set', 'BackStretchImg', $image );
 }
 //* Hook entry background area
 add_action( 'genesis_after_header', 'grvrocks_entry_background' );
 function grvrocks_entry_background() {
-	$postid = get_the_ID();
 	if ( ( false == get_post_format($postid) || 'aside' == get_post_format($postid) ) && has_post_thumbnail($postid) ) {
 		echo '<div class="entry-background"><div class="wrap">';
 		echo genesis_do_post_title();
