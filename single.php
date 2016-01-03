e317882c6525d079c35bbadeba83f6f2504bb116
Gaurav Pareek <me@grv.rocks>
2016-01-03 22:44:23 +0530

    yay for cleaner solution

--- single.php
+++ single.php
@@ -11,7 +11,7 @@ function grvrocks_set_background_image() {
 add_action( 'genesis_after_header', 'grvrocks_entry_background' );
 function grvrocks_entry_background() {
 	if ( ( false == get_post_format($postid) || 'aside' == get_post_format($postid) ) && has_post_thumbnail($postid) ) {
-		echo '<div class="entry-background"><div class="dark-gradient"><div class="wrapper">';
+		echo '<div class="entry-background"><div class="dark-gradient"><div class="wrap">';
 		echo genesis_post_meta();
 		echo genesis_do_post_title();
 		if ( function_exists( 'the_subtitle' ) ) {
