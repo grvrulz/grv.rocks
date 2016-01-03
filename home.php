0c5917ed072c895a055e99ae4bb5855b75214568
Gaurav Pareek <me@grv.rocks>
2016-01-03 08:44:57 +0530

    removed goofy grid stuff

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ function rgc_blog_intro() {
 	if ( is_null( $posts_page ) ) {
 		return;
 	}
-    $posts_page = get_post( $posts_page );
+	$posts_page = get_post( $posts_page );
 	$title   = $posts_page->post_title;
 	$content = $posts_page->post_content;
 	$title_output = $content_output = '';
@@ -30,33 +30,33 @@ function rgc_blog_intro() {
 }
 
 //* Reposition the entry meta in the entry header
-remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
-add_action( 'genesis_entry_header', 'genesis_do_post_title', 13 );
-
-//* Add grvrocks grid body class
-add_filter( 'body_class', 'grvrocks_grid_body_class' );
-function grvrocks_grid_body_class( $classes ) {
-
-	$classes[] = 'grvrocks-grid';
-	return $classes;
-
-}
-
-//* Remove entry content
-if ( 'link' != get_post_format() || 'quote' != get_post_format() ) {
-	remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
-}
-
-//* Remove entry meta in entry footer
-remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
-remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-
-
-add_theme_support( 'genesis-structural-wraps', array(
-	'header',
-	'footer',
-	'site-inner'
-) );
+//remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+//add_action( 'genesis_entry_header', 'genesis_do_post_title', 13 );
+//
+////* Add grvrocks grid body class
+//add_filter( 'body_class', 'grvrocks_grid_body_class' );
+//function grvrocks_grid_body_class( $classes ) {
+//
+//	$classes[] = 'grvrocks-grid';
+//	return $classes;
+//
+//}
+//
+////* Remove entry content
+//if ( 'link' != get_post_format() || 'quote' != get_post_format() ) {
+//	remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+//}
+//
+////* Remove entry meta in entry footer
+//remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+//remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+//remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+//
+//
+//add_theme_support( 'genesis-structural-wraps', array(
+//	'header',
+//	'footer',
+//	'site-inner'
+//) );
 
 genesis();
