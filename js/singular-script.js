9edf1046260ec6cd6d6c6240721ac5257d5e2dfc
Gaurav Pareek <me@grv.rocks>
2016-01-03 13:30:07 +0530

    css tweaks for background

--- js/singular-script.js
+++ js/singular-script.js
@@ -1,8 +1,8 @@
 jQuery(function( $ ){
 
-	$('.entry-background .wrap') .css({'height': (($(window).height()))+'px'});
+	$('.entry-background') .css({'height': (($(window).height()))+'px'});
 	$(window).resize(function(){
-		$('.entry-background .wrap') .css({'height': (($(window).height()))+'px'});
+		$('.entry-background') .css({'height': (($(window).height()))+'px'});
 	});
 
 //	$(".entry-background .entry-header .entry-meta").after('<p class="arrow"><a href="#site-inner"></a></p>');
