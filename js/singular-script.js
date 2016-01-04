a9fa53a55bcdd0d9acde19064ebe39681f7590e4
Gaurav Pareek <me@grv.rocks>
2016-01-04 16:53:19 +0530

    scroll experiment

--- js/singular-script.js
+++ js/singular-script.js
@@ -1,9 +1,23 @@
 jQuery(function( $ ){
 
-	$('.entry-background') .css({'height': (($(window).height()))+'px'});
-	$(window).resize(function(){
-		$('.entry-background') .css({'height': (($(window).height()))+'px'});
+//	$('.entry-background') .css({'height': (($(window).height()))+'px'});
+//	$(window).resize(function(){
+//		$('.entry-background') .css({'height': (($(window).height()))+'px'});
+//	});
+
+	$(window).scroll(function(){
+		var header = $('.site-header'),
+			scroll = $(window).scrollTop();
+
+		if( scroll >= 300 ) {
+			header.addClass('scrolled');
+		} else {
+			header.removeClass('scrolled');
+		}
+
 	});
+
+
 
 //	$(".entry-background .entry-header .entry-meta").after('<p class="arrow"><a href="#site-inner"></a></p>');
 
